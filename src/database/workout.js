1f9bfe3f8513f71dea0a9176221ef128bd97c23c
abdikeem <abdihakimabdirahman21@gmail.com>
2022-09-05 12:17:59 +0300

    Api

---
+++ src/database/workout.js
@@ -0,0 +1,30 @@
+const DB = require('./db.json');
+
+const getAllWorkouts = () => {
+    const allWorkouts = workouts.getAllWorkouts();
+    return allWorkouts;
+};
+
+const getOneWorkout = () => {
+    return;
+  };
+  
+  const createNewWorkout = () => {
+    return;
+  };
+  
+  const updateOneWorkout = () => {
+    return;
+  };
+  
+  const deleteOneWorkout = () => {
+    return;
+  };
+  
+  module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
+  };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const v1WorkoutRouter = require('./v1/routes/workoutRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// for testing purposes only
-app.get('/', (req, res) => {
-    res.send("<h2>It's working!</h2>");
-});
+app.use('/api/v1/workout', v1WorkoutRouter);
 
 app.listen(PORT), () => {
-    console.log("API listening on port " + port);
+    console.log(`API is listening on port ${PORT}`);
 };
